bc47043b7ed5b4984e61ac50f5f977c556a86dd5
Tianzhi Huang <th2888@columbia.edu>
2022-04-18 00:48:13 -0400

    Tianzhi Huang's responsible parts of HW11 done and commit

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ COMS4170 UI Design Project
 Weight Lifting
 
 Jaesung Park, Tianzhi Huang, Kevin Kearney, Carlos Gonzalez-Romero
+
+
+Updates on 04/18/2022 done by Tianzhi Huang:
+
+1. Completed UI design of routes: /quizhome, /quiz/<id>, /score 
+2. Modified layout.html to include navibar link of Quiz
+3. Completed backend data structure creations related to Quiz
+4. Completed Quiz-related pages redirections, as well as related data transfer(quiz score, pending quiz, quiz id...)
+5. Completed Quiz-related navigation functionalities.
+6. TODO: having media lile picture and video included (will be done in future HWs)
+
+Modified files:
+
+server.py
+layout.html
+quizhome.html
+quiz.html
+score.html
+quizhome.js
+quiz.js
+score.js
+ 
--- server.py
+++ server.py
@@ -8,14 +8,49 @@ app = Flask(__name__)
 
 #LESSON USER DATA
 
+# ---------------------------created by Tianzhi Huang Start----------------------------------------------
 
-#QUIZ TEMPLATE DATA
-review_data = {'triceps':'triceps review','delts':'delts review','pecs':'pecs review'}
+# dict recording user's answer of each question
+user_answers = {}
 
-#QUIZ USER DATA
-quiz_wrong_stack =[] #store here from grade_quiz
+# quiz question detail information
+quiz_data = {
+    "1": {
+        "id": 1,
+        "content": "What do you notice is different about this and your pushup based on triceps?",
+        "options": {1: "Shoulder and Elbow Angle", 2: "Feet", 3: "Stomach", 4: "Other"},
+        "correct_answer": 1,
+        "is_answered": 0,
+        "user_answer": 0,
+        "result": ""
+    },
+    "2": {
+        "id": 2,
+        "content": "What do you notice is different about this and your pushup based on delts?",
+        "options": {1: "Shoulder Tension", 2: "Head Angle", 3: "Hand Angle", 4: "Other"},
+        "correct_answer": 2,
+        "is_answered": 0,
+        "user_answer": 0,
+        "result": ""
+    },
+    "3": {
+        "id": 3,
+        "content": "What do you notice is different about this and your pushup based on pecs?",
+        "options": {1: "Shoulder and Elbow Angle", 2: "Hand Angle", 3: "Shoulder Tension", 4: "Other"},
+        "correct_answer": 2,
+        "is_answered": 0,
+        "user_answer": 0,
+        "result": ""
+    }
+}
 
+# recording how many questions the user answers correctly
+correct_answer_count = 0
 
+# recording the pending quiz id
+current_quiz_id = 1
+
+# ---------------------------created by Tianzhi Huang End----------------------------------------------
 
 
 # ROUTES
@@ -28,40 +63,99 @@ def welcome():
 def lesson():
     return render_template('lesson.html')
 
-@app.route('/quiz')
-def quiz():
-    return render_template('quiz.html') #insert var here if needed
-#we need to keep track of what they clicked in the quiz, send to grade_quiz, which will..
-#..redirect to review, which template will fill out with a quiz relevant to what they got..
-#..wrong (in a stack fashion) and when they get it perfect or this quiz<->review happens..
-#..3 times, we show scorescreen
 
-@app.route('/review')
-def review():
-    return render_template('review.html') #insert var here if needed
 
-@app.route('/grade_quiz', methods=['GET','POST'])
-def grade_quiz(ID=None):
-    '''
-    global data
-    json_data = request.get_json()   
-    editEntry = json_data['data']
-    editEntry['tags']=(editEntry['tags']).split(",")
-    editEntry['similar']=(editEntry['similar']).split(",")
-    i=0
-    for dic in data:
-        #print("check1 ", type(dic['id']), type(editEntry['id']))
-        if dic['id'] == editEntry['id']:
-            print(data)
-            data[i] = editEntry
-        i+=1
-    return jsonify(data=data)
-    '''
-    pass
 
+# ---------------------------created by Tianzhi Huang Start----------------------------------------------
+
+# quiz home page route
+@app.route('/quizhome')
+def quiz_home():
+    return render_template('quizhome.html')
+
+# quiz page route
+@app.route('/quiz/<id>')
+def quiz(id=None):
+
+    global quiz_data
+    global correct_answer_count
+    global current_quiz_id
+    
+    quiz = quiz_data[str(id)]
+    total = len(quiz_data)
+    
+    return render_template('quiz.html',quiz=quiz, total=total)
+
+# submit quiz answer and record the answer
+@app.route('/grade', methods=['POST'])
+def grade():
+    global quiz_data
+    global correct_answer_count
+    global current_quiz_id
+    global user_answers
+
+    request_data = request.get_json()
+    quiz_id = int(request_data["quiz_id"])
+    user_answer = int(request_data["user_answer"])
+    current_quiz_id = quiz_id + 1
+    result = ""
+
+    if user_answer == quiz_data[str(quiz_id)]["correct_answer"]:
+        result = "Congratulation, You got the correct answer! Click next to proceed to the next question."
+        quiz_data[str(quiz_id)]["result"] = result
+        correct_answer_count += 1
+    else:
+        result = "You are almost there! The correct answer is [" + quiz_data[str(quiz_id)]["options"][quiz_data[str(quiz_id)]["correct_answer"]] + "]. Click next to proceed to the next question."
+        quiz_data[str(quiz_id)]["result"] = result
+
+    quiz_data[str(quiz_id)]["is_answered"] = 1
+    quiz_data[str(quiz_id)]["user_answer"] = user_answer
+    user_answers[str(quiz_id)] = user_answer
+
+    return jsonify(result=result)
+
+# restart the whole quiz
+@app.route('/restart', methods=['GET'])
+def restart():
+    global quiz_data
+    global correct_answer_count
+    global current_quiz_id
+    global user_answers
+
+    correct_answer_count = 0
+    current_quiz_id = 1
+    user_answers.clear()
+    for id, value in quiz_data.items():
+        value["is_answered"] = 0
+        value["user_answer"] = 0
+        value["result"] = ""
+
+    return jsonify(code=200)
+
+# retrive the pending quiz id
+@app.route('/getPendingQuizId', methods=['GET'])
+def getPendingQuizId():
+    
+    global current_quiz_id
+    global quiz_data
+
+    if current_quiz_id > len(quiz_data):
+        return jsonify(id=-1)
+
+    return jsonify(id=current_quiz_id)
+
+# retrive the overall score        
 @app.route('/score')
 def score():
-    return render_template('score.html') #insert var here if needed
+    global quiz_data
+    global correct_answer_count
+
+    score = correct_answer_count
+    total = len(quiz_data)
+    
+    return render_template('score.html',score=score, total=total) #insert var here if needed
+
+# ---------------------------created by Tianzhi Huang End----------------------------------------------
 
 
 if __name__ == '__main__':
